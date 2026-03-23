eb65c61f07dbe181c32e7416d168825b3c36d51b
MICHAEL REYSSENT <mreyssentt@gmail.com>
2026-03-23 18:52:38 +0700

    feat: shifting sidebar html to components

---
+++ components/sidebar.js
@@ -0,0 +1,13 @@
+document.getElementById('sidebar').innerHTML = `
+    <aside class="sidebar">
+        <a href="/index.html" id="company-logo">COMPANY LOGO</a>
+        <a href="/index.html">HOME</a>
+        <a href="/pages/assets.html">ASSETS</a>
+        <a href="/pages/sales.html">SALES</a>
+        <a href="/pages/inventory.html">INVENTORY</a>
+        <a href="/pages/expense.html">EXPENSE</a>
+        <a href="/pages/history.html">HISTORY</a>
+        <a href="/pages/adjustment.html">ADJUSTMENT</a>
+        <a href="/index.html" id="logout">LOGOUT</a>
+    </aside>
+`;
